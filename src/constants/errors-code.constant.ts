4bb1c379f098bf0a65c3aa0ecfe236451d036c0f
samsif <samet.development@gmail.com>
2020-12-25 15:03:18 +0100

    refactoring

--- src/constants/errors-code.constant.ts
+++ src/constants/errors-code.constant.ts
@@ -2,5 +2,4 @@ export const BAD_REQUEST_CODE = '400';
 export const CONFLICT_CODE = '409';
 export const INAUTHORIZED_CODE = '403';
 export const NOT_FOUND_CODE = '404';
-export const DB_ERROR = '500';
 export const DB_CONFLICT = 'duplicate key error';
