961bb352065f6a653f235746c19b79fef3a4304c
samsif <samet.development@gmail.com>
2021-01-05 13:31:57 +0100

    dockerize node back prod mode

--- src/constants/api-const.ts
+++ src/constants/api-const.ts
@@ -3,6 +3,6 @@ export const ROOM_URL = '/rooms';
 export const CHAT_URL = '/chat';
 export const VERSION_URL = '/version';
 export const ROOM_ACCESS_URL = '/rooms/access';
-export const EXPRESS_API_PORT = process.env.NODE_ENV === 'dev' ? 3000 : 3005;
-export const SOCKET_API_PORT = process.env.NODE_ENV === 'dev' ? 3002 : 3007;
+export const EXPRESS_API_PORT = 3000;
+export const SOCKET_API_PORT = 3002;
 
--- src/constants/database.constant.ts
+++ src/constants/database.constant.ts
@@ -1,6 +1,7 @@
 export const MONGO_DB = '/local';
 export const MONGO_PORT = process.env.NODE_ENV === 'dev' ? 27018 : 27019;
-export const MONGO_BASE_URL = 'mongodb://localhost:';
+const MONGO_SERVICE_NAME = process.env.NODE_ENV === 'dev' ? 'mongo' : 'mongo-prod';
+export const MONGO_BASE_URL = `mongodb://${MONGO_SERVICE_NAME}:`;
 export const MONGO_CONNECTION_OPTIONS = {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true};
 export const USERS_COLLECTION = 'users';
 export const CHAT_COLLECTION = 'chat';
